4b405dc2109788e9af204c12bf9c601f36f410c8
siweiZhu <thinkzhu110929@gmail.com>
2021-01-07 22:57:38 +0800

    Initial Commit

---
+++ SwiftScrollView/SwiftScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftScrollViewApp.swift
+//  SwiftScrollView
+//
+//  Created by siweiZhu on 2021/1/7.
+//
+
+import SwiftUI
+
+@main
+struct SwiftScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
